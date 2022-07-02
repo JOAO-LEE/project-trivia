024f46b51a56fc5a1398ac4ece0087b523e8c0ca
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-07-02 00:13:35 -0300

    add-gif-silvio

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Setings.module.css';
 import Voltar from '../imgs/voltar.png';
+import Logo from '../imgs/silvio01.gif';
 
 class Settings extends React.Component {
   render() {
     return (
       <form className={ styles.configPage }>
         <div className={ styles.configBox }>
+          <div>
+            <img src={ Logo } alt="gif silvio" className={ styles.logoImg } />
+          </div>
           <div className={ styles.configTitle }>
             <Link to="/" data-testid="btn-settings">
               <img src={ Voltar } alt="voltar" className={ styles.configImg } />
@@ -17,9 +21,9 @@ class Settings extends React.Component {
           <label htmlFor="categories" className={ styles.configLabel }>
             <h3 className={ styles.h3 }>Categorias</h3>
             <select name="categories" className={ styles.select }>
-              <option value="valor1" selected>Animes</option>
-              <option value="valor2">Comidas</option>
-              <option value="valor3">Todos os assuntos</option>
+              <option value="valor1" selected>Todas as categorias</option>
+              <option value="valor2">Animes</option>
+              <option value="valor3">Comidas</option>
             </select>
           </label>
           <label htmlFor="dificult" className={ styles.configLabel }>
@@ -33,9 +37,9 @@ class Settings extends React.Component {
           <label htmlFor="type" className={ styles.configLabel }>
             <h3 className={ styles.h3 }>Tipo</h3>
             <select name="type" className={ styles.select }>
-              <option value="valor1" selected>Tipo A</option>
-              <option value="valor2">Valor 2</option>
-              <option value="valor3">Valor 3</option>
+              <option value="valor1" selected> Verdadeiro ou Falso</option>
+              <option value="valor2">Varias Escolhas</option>
+              <option value="valor3">Todos</option>
             </select>
           </label>
         </div>
